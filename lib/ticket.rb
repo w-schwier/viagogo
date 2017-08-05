5bccc50a1ab7a220497ffae3326fcf6ff22cd936
Will Schwier <w.schwier@hotmail.co.uk>
2017-08-05 23:56:24 +0100

    add ability to add random price

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket
 
 attr_reader :price
-  def initialize
-    @price = 0
+  def initialize(price: 0)
+    @price = price
   end
 
 end
--- spec/ticket_spec.rb
+++ spec/ticket_spec.rb
@@ -2,9 +2,15 @@ require 'ticket'
 
 describe Ticket do
   subject(:ticket) {described_class.new}
+  subject(:t2) {described_class.new(price: rand * (100-0.1) + 0.1)}
+
 
   it "checks ticket has zero price to start" do
     expect(ticket.price).to eq 0
+  end
+
+  it "checks tickets can have random prices" do
+    expect(t2.price).not_to eq 0
   end
 
 end

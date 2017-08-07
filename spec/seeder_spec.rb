a84d704ede72dc16ec19bd964ad5c5f5c5efebd8
Will Schwier <w.schwier@hotmail.co.uk>
2017-08-07 22:03:12 +0100

    Add new test frame

--- spec/seeder_spec.rb
+++ spec/seeder_spec.rb
@@ -3,9 +3,13 @@ require 'seeder'
 describe Seeder do
   subject(:seeder) {described_class.new}
 
-  it "gathers 10 uniquie event co-ordinates" do
+  it "gathers 10 event co-ordinates" do
     seeder.populate_ordinates
     expect(seeder.ordinates.length).to eq 10
+  end
+
+  it 'checks all co-ordinates are unique' do
+
   end
 
 end

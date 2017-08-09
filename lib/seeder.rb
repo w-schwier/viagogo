87d9afa77b063d6199124a6f71c218f8b4928f86
Will Schwier <w.schwier@hotmail.co.uk>
2017-08-09 15:50:54 +0100

    Add populate events function

--- lib/seeder.rb
+++ lib/seeder.rb
@@ -11,4 +11,15 @@ class Seeder
     @grid.make_grid
     10.times { @ordinates.push(@grid.coordinates.shuffle.pop) }
   end
+
+  def populate_events
+    id = 1
+    @ordinates.each do |x, y|
+      event = Event.new(id: id, x: x, y: y)
+      @events.push(event)
+      id += 1
+    end
+  end
+
+
 end
--- spec/seeder_spec.rb
+++ spec/seeder_spec.rb
@@ -2,15 +2,19 @@ require 'seeder'
 
 describe Seeder do
   subject(:seeder) {described_class.new}
+  let(:event) {double :event}
 
   it "gathers 10 event co-ordinates" do
     seeder.populate_ordinates
     expect(seeder.ordinates.length).to eq 10
   end
 
-  it 'checks all co-ordinates are unique' do
+  it 'check new events are created' do
     seeder.populate_ordinates
-    expect(seeder.ordinates).to eq []
+    seeder.populate_events
+    expect(seeder.events.length).to eq 10
+    expect(seeder.events[0].x).not_to eq nil
+    expect(seeder.events[9].x).not_to eq nil
   end
 
 end
